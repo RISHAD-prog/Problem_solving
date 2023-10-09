45b282b509b08f84d3b1859e798876bedb0d095c
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-09 16:38:21 +0600

    Leetcode: 136. Single Number, Runtime: 421 ms

---
+++ Leetcode Problems/C#/Integer_Break/Integer_Break/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public int MissingNumber(int[] nums)
+    {
+        var c = 0;
+        var v = 0;
+        for (int j = 0; j < nums.Length; j++)
+        {
+            c = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (v > 0 && v == nums[j])
+                {
+                    c++;
+                    break;
+                }
+                else if (nums[i] == nums[j] && i != j)
+                {
+                    c++;
+                    v = nums[j];
+                }
+
+            }
+            if (c == 0)
+            {
+                c = nums[j];
+                break;
+            }
+
+        }
+        return c;
+    }
+
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] n = { -1,-1,-2};
+        var ans = program.MissingNumber(n);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode Problems/C#/first_last_element/first_last_element/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public int[] SearchRange(int[] nums, int target)
+    {
+        int i = 0;
+        List<int> list = new List<int>();
+        int[] ans = new int[2];
+        foreach (int item in nums)
+        {
+            if (item == target)
+            {
+                list.Add(i);
+            }
+            
+            i++;
+        }
+        if (list.Count!=0)
+        {
+            ans[0] = (list[0]);
+            ans[1] = (list[list.Count - 1]);
+        }
+        else
+        {
+            ans[0] = -1;
+            ans[1] = -1;
+        }
+        return ans;
+    }
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] nums = {  };
+        int target = 0;
+        var ans=program.SearchRange(nums, target);
+        foreach (var item in ans)
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
