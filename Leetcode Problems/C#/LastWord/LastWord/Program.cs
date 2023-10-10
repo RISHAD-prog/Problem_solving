cb338deb7ff29ef58414db4b89cfd0451a577209
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-10 16:27:35 +0600

    LeetCode: 58. Length of Last Word
    Runtime: 66ms, Beats 15.43%of users with C# in Runtime

---
+++ Leetcode Problems/C#/LastWord/LastWord/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        var data = s.Split(' ');
+        var v = "";
+        int i = 1;
+        while (v == "")
+        {
+            v = data[data.Length - i];
+            i++;
+        }
+        return v.Length;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string s = "luffy is still joyboy";
+        var ans = solution.LengthOfLastWord(s);
+        Console.WriteLine(ans);
+    }   
+}
