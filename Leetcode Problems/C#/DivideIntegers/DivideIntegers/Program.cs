8005cc02a89289199a7be4cba266c1ad04b12bbc
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-11 11:09:19 +0600

    29. Divide Two Integers
    Runtime: 29ms, Beats 42.64%of users with C#. Memory: Beats 88.53%of users with C#

---
+++ Leetcode Problems/C#/DivideIntegers/DivideIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    public int Divide(int dividend, int divisor)
+    {
+        long dividendLong = dividend;
+        long divisorLong = divisor;
+
+        long result = dividendLong / divisorLong;
+
+        if (result > int.MaxValue || result < int.MinValue)
+        {
+            return result > 0 ? int.MaxValue : int.MinValue;
+        }
+
+        return (int)result;
+    }
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int dividend = 7; 
+        int divisor = -3;
+        var ans = program.Divide(dividend, divisor);
+        Console.WriteLine(ans);
+    }
+}
