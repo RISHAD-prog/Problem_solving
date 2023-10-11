9042cfb0a6c4b2d8cadd2bb669844c8d4b0b0ac7
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-11 15:53:13 +0600

    66. Plus One
    Runtime:127ms, Beats 57.41%of users with C#

---
+++ Leetcode Problems/C#/PlusOne/PlusOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Numerics;
+
+public class Program
+{
+    public int[] PlusOne(int[] digits)
+    {
+        string[] s = Array.ConvertAll(digits, x => x.ToString());
+        var s1 = String.Join("", s);
+        BigInteger number = BigInteger.Parse(s1);
+        number++;
+        string result = number.ToString();
+        int[] arr = result.Select( c => int.Parse(c.ToString())).ToArray();
+        return arr;
+        /*char[] intList = {};
+        digits[digits.Length - 1] = v+1;
+            
+            intList = s.ToCharArray();
+            int[] n = new int[intList.Length];*/
+            
+           
+        
+        
+       // return n;
+    }
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] digits = { 9 };
+        var ans = program.PlusOne(digits);
+        foreach (var item in ans)
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
