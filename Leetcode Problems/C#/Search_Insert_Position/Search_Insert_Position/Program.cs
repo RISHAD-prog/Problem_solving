692c8cfbe4a63823d07b486ef37c23918b717417
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-17 12:45:34 +0600

    35. Search Insert Position
    Memory: 39.23MB, Beats 88.68%of users with C#

---
+++ Leetcode Problems/C#/Search_Insert_Position/Search_Insert_Position/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l =0;int h = nums.Length-1;
+        int mid;
+        while(l <= h)
+        {
+            mid = l + (h - l) / 2;
+            if (target < nums[mid])
+            {
+                h = mid-1;
+            }
+            else if (target > nums[mid])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+           
+        }
+
+        return l;
+    }
+    public static void Main(String[] args)
+    {
+        Program program = new Program();
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int target = 0;
+        var ans = program.SearchInsert(nums, target);   
+        Console.WriteLine(ans); 
+    }
+}
