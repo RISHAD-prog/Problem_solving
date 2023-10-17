e46aac99061963e106c8ab2aada8109f0de72cbf
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-17 11:08:42 +0600

    4. Median of Two Sorted Arrays
    Runtime: 93ms, Beats 70.15%of users with C#

---
+++ Leetcode Problems/C#/Median/Median/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] result = nums1.Concat(nums2).ToArray();
+        Array.Sort(result);
+        double median = 0.00000;
+        if (result.Length%2 != 0)
+        {
+            median = result[(result.Length) / 2] ;
+        }
+        else
+        {
+            median = result[(result.Length-1)/2] + result[((result.Length-1)/2)+1] ;
+            median = median / 2;
+        }
+        return median; 
+    }
+    public static void Main(String[] args)
+    {
+        Program program = new Program();
+        int[] nums1 = {2 };
+        int[] nums2 = {  };
+        double ans = program.FindMedianSortedArrays(nums1, nums2);
+        Console.WriteLine(ans);
+    }
+}
