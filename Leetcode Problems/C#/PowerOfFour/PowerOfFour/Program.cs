0ba01430f21f020f6172620af3904a0839ae658a
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-23 16:58:42 +0600

    342. Power of Four
    Runtime: 29ms, Beats 65.97%of users with C#

---
+++ Leetcode Problems/C#/PowerOfFour/PowerOfFour/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Solution
+{
+    public bool IsPowerOfFour(int n)
+    {
+        var i = Math.Log(n) / Math.Log(4);
+        i = Math.Truncate(i);
+        if (n > 0)
+        {
+            if ( Math.Pow(4,i) == n || n == 1)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int n = 256;
+        var ans = solution.IsPowerOfFour(n);
+        Console.WriteLine(ans);
+    }
+}
