fea31e43508012838f44a6898f5b1ca034979219
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-23 17:12:40 +0600

    231. Power of Two
    Runtime: 26ms,  Beats 83.59%of users with C#

---
+++ Leetcode Problems/C#/PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        var i = Math.Log(n) / Math.Log(2);
+        i = Math.Truncate(i);
+        if (n > 0)
+        {
+            if (Math.Pow(2, i) == n || n == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
