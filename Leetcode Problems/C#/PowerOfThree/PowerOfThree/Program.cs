89c12031da5d404c9d4cc084de88292c6c7b540c
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-23 17:19:26 +0600

    326. Power of Three
    Runtime: 48ms, Beats 35.15%of users with C#

---
+++ Leetcode Problems/C#/PowerOfThree/PowerOfThree/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        var i = Math.Log(n) / Math.Log(3);
+        i = Math.Ceiling(i);
+        if (n > 0)
+        {
+            if (Math.Pow(3, i) == n || n == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        int n = 243;
+        var ans = s.IsPowerOfThree(n);
+        Console.WriteLine(ans);
+    }
+}
