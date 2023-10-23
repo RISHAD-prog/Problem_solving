6895f374ec2b9538b29be87298461ea52628b7a3
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-23 17:37:28 +0600

    191. Number of 1 Bits

---
+++ Leetcode Problems/C#/NumberOfBits/NumberOfBits/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        int i = 0;
+        int j = 0;
+        string s = n.ToString();
+        while (i < s.Length)
+        {
+            if (s[i] ==  '1')
+            {
+                j ++;
+            }
+            i++;
+        }
+        return j;
+    }
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        uint n = 00000000000000000000000000001000;
+        var ans = s.HammingWeight(n);
+        Console.WriteLine(ans);
+    }
+}
