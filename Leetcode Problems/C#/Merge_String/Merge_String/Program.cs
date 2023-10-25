32420f79b11c94d5db67ee09ab511bb5bd020dfd
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-25 16:05:57 +0600

    1768. Merge Strings Alternately
    Runtime: 74ms, Beats 47.57%of users with C#

---
+++ Leetcode Problems/C#/Merge_String/Merge_String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        List<char> list = new List<char>();
+        int count = 0;
+        while (count < word1.Length || count <word2.Length) {
+            if (count< word1.Length)
+            {
+                list.Add(word1[count]);
+            }
+            if(count< word2.Length)
+            {
+                list.Add(word2[count]);
+            }
+            
+            count++;
+        }
+        var result = new string(list.ToArray());
+        return result;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string word1 = "ab";
+        string word2 = "pqrs";
+        var ans =  solution.MergeAlternately(word1, word2);
+        Console.WriteLine(ans);
+    }
+}
