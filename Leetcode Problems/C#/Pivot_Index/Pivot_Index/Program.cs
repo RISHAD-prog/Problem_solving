5970be26e718bb21e8be405ef283e4a6e202e784
Ismail Hossain Rishad <88015175+RISHAD-prog@users.noreply.github.com>
2023-10-26 17:09:46 +0600

    724. Find Pivot Index

---
+++ Leetcode Problems/C#/Pivot_Index/Pivot_Index/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+
+        int SumLeft = 0;
+        int SumRight = nums.Sum() - nums[0];
+        if (SumLeft == SumRight)
+            return 0;
+        for (int i = 1; i <= nums.Length - 1; i++)
+        {
+            SumLeft += nums[i];
+            SumRight -= nums[nums.Length - i];
+            if(SumRight == SumLeft) 
+                return i ;
+        }
+        return -1;
+            
+    }
+    
+  
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = { 1, 7, 3, 6, 5, 6 };
+        var ans = solution.PivotIndex(nums);
+        Console.WriteLine(ans);
+    }
+}
